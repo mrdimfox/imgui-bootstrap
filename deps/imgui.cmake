5aca9cb0136597dab2e571299e0c4050e18661a8
Lisin D <mrlisdim@gmail.com>
2021-08-23 03:44:30 +0300

    [format] Format some cmake files

--- deps/imgui.cmake
+++ deps/imgui.cmake
@@ -1,4 +1,3 @@
-
 # Backend deps
 # ============
 
@@ -6,7 +5,6 @@ find_package(glfw3 REQUIRED)
 find_package(glad REQUIRED)
 find_package(glm REQUIRED)
 find_package(OpenGL REQUIRED)
-
 
 # STB lib
 # =======
@@ -16,21 +14,22 @@ find_path(STB_INCLUDE_DIRS "stb.h")
 add_library(stb INTERFACE)
 target_include_directories(stb INTERFACE ${STB_INCLUDE_DIRS})
 
-
 # IMGUI lib
 # =========
 
 include(FetchContent)
 
-FetchContent_Declare(
-  imgui
-  GIT_REPOSITORY https://github.com/ocornut/imgui.git
-  GIT_TAG v1.84.1
-  CONFIGURE_COMMAND "" BUILD_COMMAND "")
+fetchcontent_declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG v1.84.1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+)
 
-FetchContent_GetProperties(imgui)
+fetchcontent_getproperties(imgui)
 if(NOT imgui_POPULATED)
-  FetchContent_Populate(imgui)
+    fetchcontent_populate(imgui)
 endif()
 
 add_library(imgui INTERFACE)
@@ -40,17 +39,26 @@ target_include_directories(imgui INTERFACE ${imgui_SOURCE_DIR})
 message(STATUS "Imgui location: ${imgui_SOURCE_DIR}")
 
 target_sources(
-  imgui
-  INTERFACE ${imgui_SOURCE_DIR}/imgui_widgets.cpp
-            ${imgui_SOURCE_DIR}/imgui_tables.cpp
-            ${imgui_SOURCE_DIR}/imgui_draw.cpp
-            ${imgui_SOURCE_DIR}/imgui_demo.cpp
-            ${imgui_SOURCE_DIR}/imgui.cpp
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp)
+    imgui
+    INTERFACE ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+              ${imgui_SOURCE_DIR}/imgui_tables.cpp
+              ${imgui_SOURCE_DIR}/imgui_draw.cpp
+              ${imgui_SOURCE_DIR}/imgui_demo.cpp
+              ${imgui_SOURCE_DIR}/imgui.cpp
+              ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+              ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+)
 
 target_compile_definitions(
-  imgui INTERFACE IMGUI_IMPL_OPENGL_LOADER_GLAD
-                  IMGUI_SOURCE_DIR="${imgui_SOURCE_DIR}")
+    imgui INTERFACE IMGUI_IMPL_OPENGL_LOADER_GLAD
+                    IMGUI_SOURCE_DIR="${imgui_SOURCE_DIR}"
+)
 
-target_link_libraries(imgui INTERFACE glfw glad::glad glm::glm OpenGL::GL stb)
+target_link_libraries(
+    imgui
+    INTERFACE glfw
+              glad::glad
+              glm::glm
+              OpenGL::GL
+              stb
+)
